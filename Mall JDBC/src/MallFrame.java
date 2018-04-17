0cf350859d7002ac95d7ae74b1e591f32ae29827
Mikayla Crawford <crawfordm1097@gmail.com>
2018-04-16 21:26:13 -0400

    Add files via upload

--- Mall JDBC/src/MallFrame.java
+++ Mall JDBC/src/MallFrame.java
@@ -9,16 +9,7 @@ import java.util.List;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
-
-
-/* TODO:
-2) Add functionality for normal user input restriction queries (make result appear in separate dialog)
-  -input for every string attribute (and ids), numeric range for prices (maybe just inputs) -> maybe just sort by low to high
-  -Refinements aren't made until search is clicked
-  -Updates aren't made until update is clicked (editing existing tuple will be from directly editable cells; need to separate between add new tuple and deleting)
-  -Cells can be directly edited but need to store the results in the database
-  -Need to add triggers in maria to get propagation of updates, inserts, and deletes to work
-*/
+import java.util.Comparator;
 
 /**
   * This class defines the Mall SQL Assistant.
@@ -27,9 +18,10 @@ import java.text.DateFormat;
   */
 public class MallFrame extends JFrame {
   private final int WIDTH = 800;
-  private final int HEIGHT = 620;
+  private final int HEIGHT = 640;
   private MallDAO dao;
   private JPanel mainPane;
+  private JTable[] tables;
 
   /**
     * Default constructor for the Mall GUI.
@@ -37,7 +29,10 @@ public class MallFrame extends JFrame {
   MallFrame() {
     dao = new MallDAO(); //Intializes the DOA
     mainPane = new JPanel();
+    tables = new JTable[8];
+
     mainPane.setLayout(new BorderLayout(10, 10));
+
     setTitle("Mall SQL Assistant"); //Sets title of GUI
     setSize(WIDTH, HEIGHT); //Sets size
     setupAdvanced();
@@ -66,45 +61,35 @@ public class MallFrame extends JFrame {
 
         //If a statement was typed, execute it first as an update
         if (sql != null) {
-          //Try to execute as query
           try {
-            DefaultTableModel result = dao.getTableFromQuery(sql);
-            JTable table = new JTable(result) {
-              public Dimension getPreferredScrollableViewportSize() {
-                return new Dimension(500, 150);
-              }
-            };
+            if (sql.toLowerCase().contains("select")) { //Query
+              DefaultTableModel result = dao.getTableFromQuery(sql);
+              JTable table = new JTable(result) {
+                public Dimension getPreferredScrollableViewportSize() {
+                  return new Dimension(500, 150);
+                }
+              };
 
-            //Show result
-            JOptionPane.showMessageDialog(mainPane, new JScrollPane(table), "Query Result", JOptionPane.INFORMATION_MESSAGE);
+              //Show result
+              JOptionPane.showMessageDialog(mainPane, new JScrollPane(table), "Query Result", JOptionPane.INFORMATION_MESSAGE);
+            } else { //Update
+              int rowsAffected = dao.updateQuery(sql);
+              List<String> tabNames = dao.getTableNames(); //Gets all the relation names
+
+              //Updates all tables since you don't know what the query was
+              int i = 0;
+              for (String relation : tabNames) {
+                DefaultTableModel results = dao.getTableFromQuery("SELECT * FROM " + relation);
+
+                tables[i].setModel(results);
+                i++;
+              }
+
+              JOptionPane.showMessageDialog(mainPane, "Update was made successfully. " + rowsAffected + " row(s) affected.", "Update Success", JOptionPane.INFORMATION_MESSAGE);
+            }
           } catch(MallSqlException mSql) {
             JOptionPane.showMessageDialog(mainPane, mSql.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
           }
-
-          // try {
-          //   int rowsAffected = dao.updateQuery(sql);
-          //
-          //   if (rowsAffected == 0) { //No matching rows
-          //     JOptionPane.showMessageDialog(mainPane, "No matching data found for update.", "Failure", JOptionPane.ERROR_MESSAGE);
-          //   } else { //Matching rows
-          //     JOptionPane.showMessageDialog(mainPane, "Update made successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
-          //   }
-          // } catch (MallSqlException sqle) { //Any SQLException
-          //   //Try to execute as query
-          //   try {
-          //     DefaultTableModel result = dao.getTableFromQuery(sql);
-          //     JTable table = new JTable(result) {
-          //       public Dimension getPreferredScrollableViewportSize() {
-          //         return new Dimension(500, 150);
-          //       }
-          //     };
-          //
-          //     //Show result
-          //     JOptionPane.showMessageDialog(mainPane, new JScrollPane(table), "Query Result", JOptionPane.INFORMATION_MESSAGE);
-          //   } catch(MallSqlException mSql) {
-          //     JOptionPane.showMessageDialog(mainPane, mSql.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-          //   }
-          // }
         }
       }
     };
@@ -127,6 +112,7 @@ public class MallFrame extends JFrame {
     */
   public void intiliazeTabs() {
     List<String> tabNames = dao.getTableNames(); //Gets all the relation names
+
     JPanel panel; //Container for holding everything for each tab
     JPanel btmPanel; //Container for holding everything below the table
 
@@ -136,10 +122,10 @@ public class MallFrame extends JFrame {
     tabbedPane.setSize(600, 200);
 
     //Used for table
-    JTable table; //Blank table to add tuples to
     JScrollPane scrollPane; //Blank scroll pane to add table to
 
     //Creates tab for each relation
+    int i = 0;
     for (String name : tabNames) {
       panel = new JPanel(); //Needed since a component can only be added once
       panel.setSize(750, 300);
@@ -147,74 +133,105 @@ public class MallFrame extends JFrame {
       btmPanel = new JPanel(new BorderLayout(45, 45));
 
       //Creates table for each relation
-      table = new JTable(dao.getTuples(name)) {
+      tables[i] = new JTable(dao.getTuples(name)) {
+        @Override
         public Dimension getPreferredScrollableViewportSize() {
           return new Dimension(750, 100);
         }
+
+        @Override
+        public boolean isCellEditable(int row, int column){
+          return false;
+        }
+
+        @Override
+        public Class<?> getColumnClass(int column) {
+          String name = this.getColumnName(column);
+
+          if (name.contains("id") || name.contains("quantity")) {
+            return Integer.class;
+          } else if (name.contains("salary") || name.contains("balance") || name.contains("price")) {
+            return java.math.BigDecimal.class;
+          } else {
+            return String.class;
+          }
+        }
       };
 
-      table.setFont(new Font("Calibri", Font.PLAIN, 15)); //Sets font for table
+      tables[i].setFont(new Font("Calibri", Font.PLAIN, 15)); //Sets font for table
+      tables[i].setAutoCreateRowSorter(true);
+      tables[i].setRowSelectionAllowed(false);
+
+      //Defines sorter for tables, used to properly differentiate between numeric and string types
+      TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(tables[i].getModel());
+      tables[i].setRowSorter(sorter);
 
       //Adds table to scrollPane (needed for attr names to be seen) and adds border
-      scrollPane = new JScrollPane(table);
+      scrollPane = new JScrollPane(tables[i]);
       scrollPane.setBorder(BorderFactory.createLineBorder(Color.black)); //Adds border to scroll pane
       panel.add(scrollPane);
 
       //Creating radio buttons for each attribute
       List<String> colNames = dao.getAllColumns(name); //Gets all the column names for the specific relation
 
+      int columnCount = tables[i].getColumnModel().getColumnCount();
       JPanel rBtnPanel = new JPanel();
+      JPanel inputPanel = new JPanel(new GridLayout(columnCount, 3, 25, 10));
+      JTextField inputs[] = new JTextField[columnCount];
+      JComboBox dropdowns[] = new JComboBox[columnCount];
+      String eqlsVals[] = new String[]{"=", "<", ">"};
 
-      //Creates radio buttons for each attribute
+      //Creates radio buttons and input for each attribute
       int count = 0; //Tracks index of curr column
       for (String attr : colNames) {
-
         //Create and add button initliazed to selected
         JRadioButton btn = new JRadioButton(attr);
         btn.setSelected(true);
-        btn.addActionListener(new ColBtnAction(table, table.getColumnModel().getColumn(count), true)); //Tie listener
+        btn.addActionListener(new ColBtnAction(tables[i], tables[i].getColumnModel().getColumn(count), true)); //Tie listener
         rBtnPanel.add(btn);
+
+        //Creates and adds inputs with labels and dropdowns
+        JPanel comboPanel = new JPanel();
+        inputPanel.add(new JLabel(attr.substring(0, 1).toUpperCase() + attr.substring(1))); //Attr label
+        dropdowns[count] = new JComboBox(eqlsVals);
+        inputPanel.add(dropdowns[count]);
+        inputs[count] = new JTextField(10);
+        inputPanel.add(inputs[count]);
+
+        //Makes numeric types sorted as numerics not alphabetical
+        Class cls = tables[i].getColumnClass(count);
+
+        if (Integer.class.isAssignableFrom(cls)) {
+          sorter.setComparator(count, new IntComparator());
+        } else if(java.math.BigDecimal.class.isAssignableFrom(cls)) {
+          sorter.setComparator(count, new DblComparator());
+        }
 
         count++; //Go to next col
       }
 
       btmPanel.add(rBtnPanel, BorderLayout.NORTH);
-
-      JPanel inputPanel = new JPanel(new GridLayout(count, 1, 0, 10));
-      JTextField inputs[] = new JTextField[count];
-      // inputPanel.setBackground(new Color(184,207,229)); //0xB8CFE5
-
-      //Creates inputs for each attribute
-      int ind = 0;
-      for (String attr : colNames) {
-        inputPanel.add(new JLabel(attr.substring(0, 1).toUpperCase() + attr.substring(1))); //Attr label
-        inputs[ind] = new JTextField(10);
-
-        inputPanel.add(inputs[ind]);
-        ind++;
-      }
-
-      // ********************** UNDO FOR INPUT PANEL **********************
-      // btmPanel.add(inputPanel, BorderLayout.CENTER);
+      btmPanel.add(inputPanel, BorderLayout.CENTER);
 
       JPanel btnPanel = new JPanel();
-      // btnPanel.setBackground(new Color(184,207,229)); //0xB8CFE5
 
       //Creates search button
       JButton searchBtn = new JButton("Search");
-
       searchBtn.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
           try {
             boolean typed = false;
             String sql = "SELECT * FROM " + name + " WHERE ";
 
+            //Builds WHERE clause
             int i = 0;
             for (String attr : colNames) {
+              Boolean stringAttr = String.class.isAssignableFrom(tables[tabbedPane.getSelectedIndex()].getColumnClass(i));
+
               if (typed && !inputs[i].getText().isEmpty()) {
-                sql += " AND " + attr + " LIKE '%" + inputs[i].getText() + "%'";
+                sql += " AND " + attr + dropdowns[i].getSelectedItem() + (stringAttr ? "'" : "") + inputs[i].getText() + (stringAttr ? "'" : "");
               } else if (!inputs[i].getText().isEmpty()) {
-                sql += attr + " LIKE '%" + inputs[i].getText() + "%'";
+                sql += attr + dropdowns[i].getSelectedItem() + (stringAttr ? "'" : "") + inputs[i].getText() + (stringAttr ? "'" : "");
                 typed = true;
               }
               i++;
@@ -233,103 +250,89 @@ public class MallFrame extends JFrame {
               JOptionPane.showMessageDialog(mainPane, new JScrollPane(table), "Results", JOptionPane.INFORMATION_MESSAGE);
             }
           } catch (MallSqlException sqle) {
-            JOptionPane.showMessageDialog(mainPane, sqle.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(mainPane, "Unfortunately, this search cannot be made.", "Error", JOptionPane.ERROR_MESSAGE);
           }
         }
       });
 
-      //Creates insert button
-      //TODO
-      JButton insertBtn = new JButton("Insert");
-
-      insertBtn.addActionListener(new ActionListener() {
+      //Creates update button
+      JButton updateBtn = new JButton("Update");
+      updateBtn.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
           try {
             boolean typed = false;
-            String sql = "INSERT INTO " + name + " VALUES (";
+            List<String> colNames = dao.getAllColumns(name); //Gets all the column names for the specific relation
+            String endSql = "";
 
+            //Builds WHERE clause
             int i = 0;
             for (String attr : colNames) {
+              Boolean stringAttr = String.class.isAssignableFrom(tables[tabbedPane.getSelectedIndex()].getColumnClass(i));
+
               if (typed && !inputs[i].getText().isEmpty()) {
-                sql += ", " + inputs[i].getText();
+                endSql += " AND " + attr + dropdowns[i].getSelectedItem() + (stringAttr ? "'" : "") + inputs[i].getText() + (stringAttr ? "'" : "");
               } else if (!inputs[i].getText().isEmpty()) {
-                sql += inputs[i].getText();
+                endSql += attr + dropdowns[i].getSelectedItem() + (stringAttr ? "'" : "") + inputs[i].getText() + (stringAttr ? "'" : "");
                 typed = true;
               }
+
               i++;
             }
 
-            sql += ")";
-
-            //If an input was typed
+            //If input was typed
             if (typed) {
-              dao.updateQuery(sql);
+              String[] colNameArr = new String[colNames.size()];
+              String[] eqlArr = new String[]{"=", "!=", "<", ">"};
 
-              //Show success
-              JOptionPane.showMessageDialog(mainPane, "Row added successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+              //Move from list to array
+              int j = 0;
+              for (String colName : colNames) {
+                colNameArr[j] = colName;
+                j++;
+              }
+
+              //Create update popup
+              JPanel updatePanel = new JPanel();
+              JComboBox colDropDown = new JComboBox(colNameArr);
+              JTextField newInput = new JTextField(5);
+
+              updatePanel.add(new JLabel("Update"));
+              updatePanel.add(colDropDown);
+              updatePanel.add(new JLabel("to"));
+              updatePanel.add(newInput);
+
+              JComponent[] options = new JComponent[] {updatePanel};
+
+              int result = JOptionPane.showConfirmDialog(mainPane, options, "Update", JOptionPane.PLAIN_MESSAGE);
+
+              //If an new value was typed and user presses ok
+              if (result == 0 && !newInput.getText().isEmpty()) {
+                String col = (String)colDropDown.getSelectedItem();
+                String sql = "UPDATE " + name + " SET " + col + " = '" + newInput.getText() + "' WHERE " + endSql;
+
+                int rowsAffected = dao.updateQuery(sql);
+                DefaultTableModel results = dao.getTableFromQuery("SELECT * FROM " + name);
+                tables[tabbedPane.getSelectedIndex()].setModel(results);
+
+                //Show success
+                JOptionPane.showMessageDialog(mainPane, "Update was made successfully. " + rowsAffected + " row(s) affected.", "Update Success", JOptionPane.INFORMATION_MESSAGE);
+              }
             }
           } catch (MallSqlException sqle) {
-            JOptionPane.showMessageDialog(mainPane, sqle.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(mainPane, "Unfortunately, this update cannot be made.", "Error", JOptionPane.ERROR_MESSAGE);
           }
         }
       });
 
-      //Creates insert button
-      //TODO test
-      JButton deleteBtn = new JButton("Delete");
+      btnPanel.add(searchBtn);
+      btnPanel.add(updateBtn);
 
-      deleteBtn.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          try {
-            boolean typed = false;
-            String sql = "DELETE FROM " + name + " WHERE ";
-
-            int i = 0;
-            for (String attr : colNames) {
-              if (typed && !inputs[i].getText().isEmpty()) {
-                sql += " AND " + attr + " LIKE '%" + inputs[i].getText() + "%'";
-              } else if (!inputs[i].getText().isEmpty()) {
-                sql += attr + " LIKE '%" + inputs[i].getText() + "%'";
-                typed = true;
-              }
-              i++;
-            }
-
-            //If an input was typed
-            if (typed) {
-              //Confirm deletion
-              Object[] options = {"DELETE", "CANCEL"};
-              int confirm = JOptionPane.showOptionDialog(null, "Are you sure you want to delete data?", "Warning",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
-                null, options, options[0]);
-
-              if (confirm == 0) { //Deletion confirmed
-                int rowsDeleted = dao.updateQuery(sql);
-
-                if (rowsDeleted != 0) { //Show success
-                  JOptionPane.showMessageDialog(mainPane, "Row deleted successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
-                } else { //Show failure
-                  JOptionPane.showMessageDialog(mainPane, "No matching data found.", "Failure", JOptionPane.ERROR_MESSAGE);
-                }
-              }
-            }
-          } catch (MallSqlException sqle) {
-            JOptionPane.showMessageDialog(mainPane, sqle.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-          }
-        }
-      });
-
-
-      //********************** UNDO FOR UPDATE BUTTONS **********************
-      // btnPanel.add(searchBtn);
-      // btnPanel.add(insertBtn);
-      // btnPanel.add(deleteBtn);
-      //
-      // btmPanel.add(btnPanel, BorderLayout.SOUTH);
+      btmPanel.add(btnPanel, BorderLayout.SOUTH);
 
       panel.add(btmPanel);
 
       tabbedPane.addTab(name.substring(0, 1).toUpperCase() + name.substring(1), panel);
+      i++;
     }
 
 
@@ -348,5 +351,37 @@ public class MallFrame extends JFrame {
     footer.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 0));
 
     mainPane.add(footer, BorderLayout.SOUTH);
+  }
+
+  /**
+    * This class defines a comparator for an integer type. It is meant to be
+    * used for all ids and quantity types.
+    */
+  class IntComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+      Integer int1 = (Integer)o1;
+      Integer int2 = (Integer)o2;
+      return int1.compareTo(int2);
+    }
+
+    public boolean equals(Object o2) {
+      return this.equals(o2);
+    }
+  }
+
+  /**
+    * This class defines a comparator for a java.lang.BigDecimal type. It is meant to be used
+    * for monetary types (salary, balance, and price).
+    */
+  class DblComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+      java.math.BigDecimal int1 = (java.math.BigDecimal)o1;
+      java.math.BigDecimal int2 = (java.math.BigDecimal)o2;
+      return int1.compareTo(int2);
+    }
+
+    public boolean equals(Object o2) {
+      return this.equals(o2);
+    }
   }
 }
